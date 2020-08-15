c9a411fd688a2535b4697ed5be9ea6236e4e592b
jinjun021 <49308330+jinjun021@users.noreply.github.com>
2020-08-15 11:36:28 +0800

    Update install.sh

--- mkcp/install.sh
+++ mkcp/install.sh
@@ -151,17 +151,23 @@ v2ray_install(){
         rm -rf /etc/v2ray
     fi
     mkdir -p /root/v2ray && cd /root/v2ray
-    wget  --no-check-certificate https://install.direct/go.sh
+   ###. wget  --no-check-certificate https://install.direct/go.sh
+    
+     wget  --no-check-certificate https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+
+wget  --no-check-certificate https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
+judge "安装 V2ray"
+bash install-release.sh --force
 
     ## wget http://install.direct/go.sh
     
-    if [[ -f go.sh ]];then
-        bash go.sh --force
-        judge "安装 V2ray"
-    else
-        echo -e "${Error} ${RedBG} V2ray 安装文件下载失败，请检查下载地址是否可用 ${Font}"
-        exit 4
-    fi
+   ###. if [[ -f go.sh ]];then
+   ###.     bash go.sh --force
+    judge "安装 V2ray"
+  ###.  else
+  ###.      echo -e "${Error} ${RedBG} V2ray 安装文件下载失败，请检查下载地址是否可用 ${Font}"
+  ###.      exit 4
+  ###.  fi
     # 清除临时文件
     rm -rf /root/v2ray
 }
@@ -172,7 +178,7 @@ v2ray_install(){
 v2ray_conf_add(){
     cd /etc/v2ray
     wget https://raw.githubusercontent.com/mikewubox/tvonekey/master/mkcp/config.json -O config.json
-modify_port_UUID
+###. modify_port_UUID
 judge "V2ray 配置修改"
 }
 
