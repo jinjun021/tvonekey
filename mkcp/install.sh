49d6361ba11349b124acdc6851315a6af4f8d958
jinjun021 <49308330+jinjun021@users.noreply.github.com>
2020-08-15 22:53:40 +0800

    Update install.sh

--- mkcp/install.sh
+++ mkcp/install.sh
@@ -144,26 +144,25 @@ modify_port_UUID(){
 
 
 v2ray_install(){
-    if [[ -d /root/v2ray ]];then
+    if [[ -d /root/v2ray ]]; then
         rm -rf /root/v2ray
     fi
-    if [[ -d /etc/v2ray ]];then
+    if [[ -d /etc/v2ray ]]; then
         rm -rf /etc/v2ray
     fi
-    mkdir -p /root/v2ray && cd /root/v2ray
-    # wget  --no-check-certificate https://install.direct/go.sh
-    wget  --no-check-certificate https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+    mkdir -p /root/v2ray
+    cd /root/v2ray || exit
+    wget -N --no-check-certificate https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/v2ray.sh
     
-    if [[ -f install-release.sh ]];then
-        bash install-release.sh --force
+    if [[ -f v2ray.sh ]]; then
+        bash v2ray.sh --force
         judge "安装 V2ray"
     else
         echo -e "${Error} ${RedBG} V2ray 安装文件下载失败，请检查下载地址是否可用 ${Font}"
         exit 4
     fi
     # 清除临时文件
-    # cd /root/v2ray
-    # rm -rf /root/v2ray
+    rm -rf /root/v2ray
 }
 
 
